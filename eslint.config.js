663a41b98f1ee5b0846c7820ffe51dd29a47b815
Yangyang <yangyang@weimill.com>
2024-12-19 19:53:00 +0800

    eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,10 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     type: 'lib',
+    typescript: {
+      overrides: {
+        'ts/explicit-function-return-type': 0,
+      },
+    },
   },
 )
