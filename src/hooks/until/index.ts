1086f0387c561a78e10f1b35d56eef7f4a9625a7
Yangyang <yangyang@weimill.com>
2024-12-19 21:09:12 +0800

    enhance

--- src/hooks/until/index.ts
+++ src/hooks/until/index.ts
@@ -2,22 +2,18 @@ import type { MaybeAccessor } from '../access'
 import { observable } from 'solid-js'
 import { access } from '../access'
 
-export interface IUntilTypeReturn {
-  toBe: (v: any) => Promise<void>
-}
-
-export function until(value: MaybeAccessor): IUntilTypeReturn {
+export function until<T>(value: MaybeAccessor<T>) {
   const obsv = observable(() => access(value))
   let lastUnsubscribe: (() => void) | null
   const toBe = (v: any) => {
-    return new Promise<void>((resolve) => {
+    return new Promise<T>((resolve) => {
       if (lastUnsubscribe) {
         lastUnsubscribe()
         lastUnsubscribe = null
       }
       const { unsubscribe } = obsv.subscribe((newValue) => {
         if (newValue === v) {
-          resolve()
+          resolve(newValue)
           unsubscribe()
         }
       })
